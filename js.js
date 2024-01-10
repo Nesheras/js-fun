3213e09d66ef40609d9bc1b556fb6a77fa3bd4ea
NesherasPC <karoshop@mail.ru>
2024-01-10 18:23:04 +0300

    Работа с js

--- js.js
+++ js.js
@@ -1,8 +1,19 @@
 const getNumber = (max=11,min=0)=>Math.floor(Math.random() * (max-min)+min);
 const data = []; 
+
+const optimizeNumber = (n)=>{
+   
+    return (n + "").length === 2 ? n : "0"+ n;
+}
+
 let n = getNumber(40,1);
 while(n--){
     data.push(getNumber(100));
 
 }
 console.log(data);
+const box = document.querySelector(".box");
+for (const val of data) {
+    box.innerHTML += `<div class = "card">${optimizeNumber(val)}<div/>`;
+    
+}
